66674bc63383fa1968189c7ab2d650bf69108c3b
felixrewer <felix@rewer.com>
2018-07-15 17:15:04 +0200

    Added new components (Desktop #396)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DesktopMenuComponent } from './desktop/desktop-menu/desktop-menu.component';
+import { DesktopStartmenuComponent } from './desktop/desktop-startmenu/desktop-startmenu.component';
+import { KontextmenuComponent } from './desktop/kontextmenu/kontextmenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DesktopMenuComponent,
+    DesktopStartmenuComponent,
+    KontextmenuComponent
   ],
   imports: [
     BrowserModule
