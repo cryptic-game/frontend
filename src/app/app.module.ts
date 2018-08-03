3b3048c34f64be141286fa26a0edf817fa953962
felixrewer <felix@rewer.com>
2018-08-03 23:03:40 +0200

    Show desktop component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DesktopComponent } from './desktop/desktop.component';
 import { DesktopMenuComponent } from './desktop/desktop-menu/desktop-menu.component';
 import { DesktopStartmenuComponent } from './desktop/desktop-startmenu/desktop-startmenu.component';
 import { KontextmenuComponent } from './desktop/kontextmenu/kontextmenu.component';
@@ -11,14 +12,13 @@ import { KontextmenuComponent } from './desktop/kontextmenu/kontextmenu.componen
   declarations: [
     AppComponent,
     LoginComponent,
+    DesktopComponent,
     DesktopMenuComponent,
     DesktopStartmenuComponent,
     KontextmenuComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
