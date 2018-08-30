22b6dc5c22a397ac6f03bc79b005ed914eae5cce
MaxiHuHe04 <maxiherczegh@outlook.com>
2018-08-30 18:34:29 +0200

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DesktopComponent } from './desktop/desktop.component';
-import { DesktopMenuComponent } from './desktop/desktop-menu/desktop-menu.component';
-import { DesktopStartmenuComponent } from './desktop/desktop-startmenu/desktop-startmenu.component';
-import { ContextMenuComponent } from './desktop/context-menu/context-menu.component';
-import { DesktopSurfaceComponent } from './desktop/desktop-surface/desktop-surface.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {DesktopComponent} from './desktop/desktop.component';
+import {DesktopMenuComponent} from './desktop/desktop-menu/desktop-menu.component';
+import {DesktopStartmenuComponent} from './desktop/desktop-startmenu/desktop-startmenu.component';
+import {ContextMenuComponent} from './desktop/context-menu/context-menu.component';
+import {DesktopSurfaceComponent} from './desktop/desktop-surface/desktop-surface.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: DesktopComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
@@ -22,8 +30,9 @@ import {SignUpComponent} from './sign-up/sign-up.component';
     DesktopSurfaceComponent,
     SignUpComponent
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
