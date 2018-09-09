f4790f1a96e65a34bd8b4358c9a5438eccf9798f
MaxiHuHe04 <maxiherczegh@outlook.com>
2018-09-09 10:58:45 +0200

    Removed intl

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,6 +78,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-import 'intl';
-import 'intl/locale-data/jsonp/en';
