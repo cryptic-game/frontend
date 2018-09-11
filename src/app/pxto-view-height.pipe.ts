68511109ec73c5d9d8e1d6aad59793d9fc6a6b44
felixrewer <felix@rewer.com>
2018-09-11 16:37:16 +0200

    Corrected typo

--- src/app/pxto-view-height.pipe.ts
+++ src/app/pxto-view-height.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PXtoViewHeightPipe implements PipeTransform {
 
   transform(value: number): number {
-    return value * (100 / document.documentElement.clientHeightc);
+    return value * (100 / document.documentElement.clientHeight);
   }
 
 }
