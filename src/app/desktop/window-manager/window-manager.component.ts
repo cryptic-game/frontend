452bc39001d36cd99ecd3157ea494d31c9d10e60
MaxiHuHe04 <maxiherczegh@outlook.com>
2018-12-08 21:46:38 +0100

    Added window manager component to the desktop

--- src/app/desktop/window-manager/window-manager.component.ts
+++ src/app/desktop/window-manager/window-manager.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {WindowManagerService} from './window-manager.service';
+import {TestWindowComponent} from '../windows/test-window/test-window.component';
 
 @Component({
   selector: 'app-window-manager',
@@ -11,6 +12,10 @@ export class WindowManagerComponent implements OnInit {
   constructor(protected windowManager: WindowManagerService) { }
 
   ngOnInit() {
+    this.windowManager.openWindow(new TestWindowComponent());
+    this.windowManager.openWindow(new TestWindowComponent());
+    this.windowManager.openWindow(new TestWindowComponent());
+    this.windowManager.openWindow(new TestWindowComponent());
   }
 
 }
