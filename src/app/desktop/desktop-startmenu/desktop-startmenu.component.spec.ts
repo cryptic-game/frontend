a37a420e9652523339d729a190f146f8e3e1e48f
MaxiHuHe04 <maxiherczegh@outlook.com>
2019-02-02 18:36:28 +0100

    Fixed start menu test

--- src/app/desktop/desktop-startmenu/desktop-startmenu.component.spec.ts
+++ src/app/desktop/desktop-startmenu/desktop-startmenu.component.spec.ts
@@ -1,9 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DesktopStartmenuComponent } from './desktop-startmenu.component';
+import {DesktopStartmenuComponent} from './desktop-startmenu.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
+import {DesktopComponent} from '../desktop.component';
+import {PXtoViewHeightPipe} from '../../pxto-view-height.pipe';
+import {PXtoViewWidthPipe} from '../../pxto-view-width.pipe';
+import {WindowManagerComponent} from '../window-manager/window-manager.component';
+import {ContextMenuComponent} from '../context-menu/context-menu.component';
+import {WindowFrameComponent} from '../window/window-frame.component';
+import {DesktopMenuComponent} from '../desktop-menu/desktop-menu.component';
 
 describe('DesktopStartmenuComponent', () => {
   let component: DesktopStartmenuComponent;
@@ -14,14 +21,24 @@ describe('DesktopStartmenuComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, HttpClientModule, RouterTestingModule],
-      declarations: [ DesktopStartmenuComponent ]
+      declarations: [
+        DesktopStartmenuComponent,
+        DesktopComponent,
+        PXtoViewWidthPipe,
+        PXtoViewHeightPipe,
+        WindowManagerComponent,
+        ContextMenuComponent,
+        WindowFrameComponent,
+        DesktopMenuComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DesktopStartmenuComponent);
     component = fixture.componentInstance;
+    component.parent = TestBed.createComponent(DesktopComponent).componentInstance;
     fixture.detectChanges();
   });
 
