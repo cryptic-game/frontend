4b8f266d7097e12fe887508985321efb21c0e8eb
FoseFx <fosefx@pm.me>
2019-03-15 14:53:48 +0100

    performSignup(): Improved error handling on expected errors, like weak passwords as well as on Network errors ('An error has occured' will be visible for the user)

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {SignUpService} from './sign-up.service';
-import {Router} from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SignUpService } from './sign-up.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,7 +13,7 @@ export class SignUpComponent implements OnInit {
   @ViewChild('passwordField') passwordField: ElementRef;
   @ViewChild('passwordConfirm') passwordConfirmField: ElementRef;
   errorText: string;
-  model = {username: '', email: '', password: '', passwordConfirm: ''};
+  model = { username: '', email: '', password: '', passwordConfirm: '' };
 
   constructor(private signUpService: SignUpService, private router: Router) {
   }
@@ -23,17 +24,24 @@ export class SignUpComponent implements OnInit {
 
   performSignup() {
     this.signUpService.signUp(this.model.username, this.model.email, this.model.password).subscribe(data => {
-        const error = data['error'];
-        if (error === undefined) {
-          this.router.navigateByUrl('/login');
-        } else {
-          this.errorText = data['error'];
-        }
+      const error = data['error'];
+      if (error === undefined) {
+        this.router.navigateByUrl('/login');
+      } else {
+        this.errorText = data['error'];
+      }
 
-      },
-      error => {
+    },
+      (error: HttpErrorResponse) => {
         console.log(error);
-
+        try {
+          this.errorText = error.error.message; // error.error referes to the backend response, or to the Browser's Error Message, can be undefined
+        } catch (e) {
+          this.errorText = 'An error has occurred';
+        }
+        if (this.errorText === undefined) {
+          this.errorText = 'An error has occurred';
+        }
         this.loginButton.nativeElement.disabled = true;
         setTimeout(
           () => (this.loginButton.nativeElement.disabled = false),
