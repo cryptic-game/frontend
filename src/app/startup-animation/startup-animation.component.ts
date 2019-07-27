28862b6566cd78f3c9dc687c81e0d4f1933a9c52
Merlin Moelter <joebau.0815@googlemail.com>
2019-07-27 20:04:12 +0200

    Add percentage indicator, make animation modular

--- src/app/startup-animation/startup-animation.component.ts
+++ src/app/startup-animation/startup-animation.component.ts
@@ -9,36 +9,103 @@ import anime from "animejs"
 export class StartupAnimationComponent implements OnInit{
     @ViewChild("circle", {static: true}) circle: ElementRef
     @ViewChild("drop", {static: true}) drop: ElementRef
+    @ViewChild("display", {static: true}) display: ElementRef
 
     ngOnInit(){
-        const tl = anime.timeline()
-        const shadow = {size: 0}
         const drop = this.drop.nativeElement
         const circle = this.circle.nativeElement
+        const display = this.display.nativeElement
 
-        tl.add({
-            targets: ".drop-wrapper",
-            rotate: 360,
-            duration: 2000,
-            easing: () => time => (time*3.5)**2,
-            complete: () => {
-                drop.style.display = "none"
-                circle.style.border = "none"
-            }
-        }).add({
-            targets: ".drop-wrapper",
-            opacity: 0,
-            duration: 2000,
-            easing: "easeInSine",
-        }, "-=2000").add({
+        const animations = {}
+        const descriptions = {
+            loadingStates: [
+                "Loading assets...",
+                "Checking storage...",
+                "Connecting to Internet...",
+                "Starting OS..."
+            ],
+            done: "Done!"
+        }
+
+        const shadow = {size: 0}
+        const glowingDuration = 1000
+        const tl = anime.timeline({autoplay: false})
+
+        animations.initSpin = (duration = 1000) => {
+            let offset, matches = drop.style.transform.match(/\d+\.?\d+/)
+            if(matches) offset = parseInt(matches[0])
+            else offset = 0
+            animations.spin = anime({
+                targets: drop,
+                rotate: [offset, offset+360],
+                duration: duration,
+                easing: "linear",
+                autoplay: false,
+                loop: true
+            })
+        }
+        animations.initSpin()
+
+        animations.percentage = anime({
+            targets: ".percentage svg circle",
+            strokeDashoffset: [anime.setDashoffset, 0],
+            duration: 1000,
+            easing: "linear",
+            autoplay: false
+        })
+
+        animations.finish = anime({
             targets: shadow,
             size: 10,
             easing: "easeOutSine",
-            duration: 1000,
+            duration: glowingDuration,
             update: () => circle.style.boxShadow = `
                 0 0 ${shadow.size*5}px ${shadow.size/2}px #2ecc71,
                 0 0 ${shadow.size*5}px ${shadow.size/2}px #2ecc71 inset
-            `
+            `,
+            autoplay: false
         })
+
+        animations.complete = () => {
+            drop.style.display = "none"
+            circle.getElementsByTagName("circle")[0].style.display = "none"
+            display.innerHTML = descriptions.done
+            animations.finish.play()
+        }
+
+        const sleep = (t, state) => {
+            display.innerHTML = descriptions.loadingStates[state-1]
+            return new Promise(resolve => setTimeout(resolve, t))
+        }
+
+        const updatePercentage = (steps, state) => {
+            const percentage = state/steps
+            // display.innerHTML = percentage * 100 + "%"
+            animations.percentage.seek(animations.percentage.duration * percentage)
+            animations.initSpin(1000*(1/percentage)/steps)
+            animations.spin.play()
+        }
+
+        const loadData = async () => {
+            // Any kind of operation that may take a while can be called here
+            const steps = 4
+            animations.spin.play()
+
+            await sleep(750, 1)
+            updatePercentage(steps, 1)
+
+            await sleep(500, 2)
+            updatePercentage(steps, 2)
+
+            await sleep(250, 3)
+            updatePercentage(steps, 3)
+
+            await sleep(500, 4)
+            updatePercentage(steps, 4)
+
+            return Promise.resolve()
+        }
+
+        loadData().then(animations.complete)
     }
 }
