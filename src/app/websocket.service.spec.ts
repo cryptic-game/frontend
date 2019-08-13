69a593c96ac5edac305b6dfe3e5962bdbcebcf45
use-to <use-to@outlook.de>
2019-08-13 18:14:03 +0200

    fixed test dependency #59

--- src/app/websocket.service.spec.ts
+++ src/app/websocket.service.spec.ts
@@ -1,10 +1,12 @@
 import { inject, TestBed } from '@angular/core/testing';
 
 import { WebsocketService } from './websocket.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WebsocketService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [WebsocketService]
     });
   });
