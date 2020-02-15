340ad80399a0ddb83b505db53c13485f26c5584d
Marcel Koch <koch_marcel@outlook.com>
2020-02-15 22:03:24 +0100

    Added Particles. (#101)

--- src/app/account/account-page-base/account-page-base.component.ts
+++ src/app/account/account-page-base/account-page-base.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as Particles from 'particlesjs/src/particles.js';
 
 @Component({
   selector: 'app-account-page-base',
   templateUrl: './account-page-base.component.html',
   styleUrls: ['./account-page-base.component.scss']
 })
-export class AccountPageBaseComponent implements OnInit {
+export class AccountPageBaseComponent implements OnInit, OnDestroy {
+
+  private p: Particles;
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.p = Particles.init({
+      selector: 'canvas',
+      maxParticles: 450,
+      color: '#00AD00',
+      speed: 1
+    });
+    console.log(this.p);
   }
 
+  ngOnDestroy(): void {
+    this.p.destroy();
+  }
 }
