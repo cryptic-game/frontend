279bbd0f2c11b5f9dfd64663eb6a23fec40d29e2
Marcel Koch <koch_marcel@outlook.com>
2020-02-17 18:16:22 +0100

    Resolve Merge Conflicts. (#52)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './account/login/login.component';
 import { DesktopComponent } from './desktop/desktop.component';
 import { DesktopMenuComponent } from './desktop/desktop-menu/desktop-menu.component';
 import { DesktopStartmenuComponent } from './desktop/desktop-startmenu/desktop-startmenu.component';
 import { ContextMenuComponent } from './desktop/context-menu/context-menu.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './account/sign-up/sign-up.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DesktopGuard } from './desktop/desktop.guard';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginGuard } from './login/login.guard';
-import { SignUpGuard } from './sign-up/sign-up.guard';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PasswordConfirmValidatorDirective } from './sign-up/password-confirm-validator.directive';
 import { PXtoViewWidthPipe } from './pxto-view-width.pipe';
 import { PXtoViewHeightPipe } from './pxto-view-height.pipe';
 import { WindowFrameComponent } from './desktop/window/window-frame.component';
@@ -26,27 +22,22 @@ import { WindowPlaceDirective } from './desktop/window/window-place.directive';
 import { MinerComponent } from './desktop/windows/miner/miner.component';
 import { SettingsComponent } from './desktop/windows/settings/settings.component';
 import { TaskManagerComponent } from './desktop/windows/task-manager/task-manager.component';
-import { HardwareShopComponent } from './desktop/windows/hardware-shop/hardware-shop.component';
-import { HardwareShopItemComponent } from './desktop/windows/hardware-shop/hardware-shop-item/hardware-shop-item.component';
-import { HardwareShopItemGridComponent } from './desktop/windows/hardware-shop/hardware-shop-item-grid/hardware-shop-item-grid.component';
-import { HardwareShopHeaderComponent } from './desktop/windows/hardware-shop/hardware-shop-header/hardware-shop-header.component';
-// tslint:disable-next-line:max-line-length
-import { HardwareShopCartComponent } from './desktop/windows/hardware-shop/hardware-shop-cart/hardware-shop-cart.component';
-import { HardwareShopCartItemComponent } from './desktop/windows/hardware-shop/hardware-shop-cart-item/hardware-shop-cart-item.component';
-import { WalletAppComponent } from './desktop/windows/wallet-app/wallet-app.component';
-import { WalletAppHeaderComponent } from './desktop/windows/wallet-app/wallet-app-header/wallet-app-header.component';
-import { WalletAppEditComponent } from './desktop/windows/wallet-app/wallet-app-edit/wallet-app-edit.component';
-import { WalletAppTransactionComponent } from './desktop/windows/wallet-app/wallet-app-transaction/wallet-app-transaction.component';
-import { HardwareShopSidebarComponent } from './desktop/windows/hardware-shop/hardware-shop-sidebar/hardware-shop-sidebar.component';
-import { HardwareShopSidebarItemComponent } from './desktop/windows/hardware-shop/hardware-shop-sidebar-item/hardware-shop-sidebar-item.component';
 import { AccountPageBaseComponent } from './account/account-page-base/account-page-base.component';
 import { AccountGuard } from './account/account.guard';
+import { HardwareShopComponent } from './desktop/windows/hardware-shop/hardware-shop.component';
+import { HardwareShopSidebarComponent } from './desktop/windows/hardware-shop/hardware-shop-sidebar/hardware-shop-sidebar.component';
+import { HardwareShopCartComponent } from './desktop/windows/hardware-shop/hardware-shop-cart/hardware-shop-cart.component';
+import { HardwareShopCartItemComponent } from './desktop/windows/hardware-shop/hardware-shop-cart-item/hardware-shop-cart-item.component';
+import { HardwareShopHeaderComponent } from './desktop/windows/hardware-shop/hardware-shop-header/hardware-shop-header.component';
+import { HardwareShopItemComponent } from './desktop/windows/hardware-shop/hardware-shop-item/hardware-shop-item.component';
+import { HardwareShopItemGridComponent } from './desktop/windows/hardware-shop/hardware-shop-item-grid/hardware-shop-item-grid.component';
+import { HardwareShopSidebarItemComponent } from './desktop/windows/hardware-shop/hardware-shop-sidebar-item/hardware-shop-sidebar-item.component';
 
 const routes: Routes = [
   { path: '', component: DesktopComponent, canActivate: [DesktopGuard] },
-  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-  { path: 'signup', component: SignUpComponent, canActivate: [SignUpGuard] },
-  { path: '**', redirectTo: '/' }
+  { path: 'login', component: LoginComponent, canActivate: [AccountGuard] },
+  { path: 'signup', component: SignUpComponent, canActivate: [AccountGuard] },
+  { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
@@ -58,7 +49,6 @@ const routes: Routes = [
     DesktopStartmenuComponent,
     ContextMenuComponent,
     SignUpComponent,
-    PasswordConfirmValidatorDirective,
     PXtoViewWidthPipe,
     PXtoViewHeightPipe,
     WindowFrameComponent,
@@ -69,29 +59,24 @@ const routes: Routes = [
     MinerComponent,
     SettingsComponent,
     TaskManagerComponent,
+    AccountPageBaseComponent,
     HardwareShopComponent,
-    HardwareShopItemComponent,
-    HardwareShopItemGridComponent,
-    HardwareShopHeaderComponent,
+    HardwareShopSidebarComponent,
     HardwareShopCartComponent,
     HardwareShopCartItemComponent,
-    WalletAppComponent,
-    WalletAppHeaderComponent,
-    WalletAppEditComponent,
-    WalletAppTransactionComponent,
-    HardwareShopSidebarComponent,
-    HardwareShopSidebarItemComponent
-    TaskManagerComponent,
-    AccountPageBaseComponent
+    HardwareShopHeaderComponent,
+    HardwareShopItemComponent,
+    HardwareShopItemGridComponent,
+    HardwareShopSidebarItemComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  entryComponents: [TestWindowComponent, TerminalComponent, MinerComponent, SettingsComponent, TaskManagerComponent, HardwareShopComponent, WalletAppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
