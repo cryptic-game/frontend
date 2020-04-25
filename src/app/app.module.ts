80ee9fb16418c35d2dc4c913833b9dd0be08911d
CC <40364569+ClientCrash@users.noreply.github.com>
2020-04-25 21:48:25 +0200

    Added progressive web app support (#145) (#107)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { WindowManagerComponent } from './desktop/window-manager/window-manager.
 import { TestWindowComponent } from './desktop/windows/test-window/test-window.component';
 import { TerminalComponent } from './desktop/windows/terminal/terminal.component';
 import { WindowPlaceDirective } from './desktop/window/window-place.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { MinerComponent } from './desktop/windows/miner/miner.component';
 import { SettingsComponent } from './desktop/windows/settings/settings.component';
 import { TaskManagerComponent } from './desktop/windows/task-manager/task-manager.component';
@@ -88,11 +90,13 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
     ReactiveFormsModule,
     DesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 import { environmentLoader } from './environments/environmentLoader';
 
@@ -9,6 +9,12 @@ environmentLoader.then(env => {
     enableProdMode();
   }
 
-  platformBrowserDynamic().bootstrapModule(AppModule)
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .then(() => {
+      if ('serviceWorker' in navigator && environment.production) {
+        navigator.serviceWorker.register('/ngsw-worker.js').then();
+      }
+    })
     .catch(err => console.log(err));
 });
