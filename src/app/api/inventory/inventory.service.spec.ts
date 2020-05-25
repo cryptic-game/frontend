c2f5d8ee5d24205f8e0637239753f24a42f5fad6
MaxiHuHe04 <maxiherczegh@outlook.com>
2020-05-25 20:53:25 +0200

    Added an inventory service (#77)

---
+++ src/app/api/inventory/inventory.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+
+import { InventoryService } from './inventory.service';
+import * as rxjs from 'rxjs';
+import { WebsocketService } from '../../websocket.service';
+import { HardwareList, HardwareService } from '../hardware/hardware.service';
+
+describe('InventoryService', () => {
+  let service: InventoryService;
+  let webSocket;
+  let hardwareService;
+
+  beforeEach(() => {
+    webSocket = jasmine.createSpyObj('WebsocketService', ['ms']);
+    webSocket.ms.and.returnValue(rxjs.of({}));
+    hardwareService = jasmine.createSpyObj('HardwareService', ['getAvailableParts']);
+    hardwareService.getAvailableParts.and.returnValue(rxjs.of(new HardwareList()));
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: WebsocketService, useValue: webSocket },
+        { provide: HardwareService, useValue: hardwareService }
+      ]
+    });
+    service = TestBed.inject(InventoryService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
