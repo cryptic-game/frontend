7eeeaaebb9ca3baaa3e5b6bc96bd63dc8ee04946
Sophie <mara@myway.de>
2020-06-07 17:33:54 +0200

    Added text-input and functionality for opening files

--- src/app/desktop/windows/editor/editor.component.ts
+++ src/app/desktop/windows/editor/editor.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { Component, ElementRef, OnInit, Type, ViewChild } from '@angular/core';
 import { WindowComponent, WindowConstraints, WindowDelegate } from '../../window/window-delegate';
+import { FileService } from '../../../api/files/file.service';
+import { Path } from '../../../api/files/path';
+import { SafeHtml } from '@angular/platform-browser';
 
 // noinspection AngularMissingOrInvalidDeclarationInModule
 @Component({
@@ -9,13 +12,43 @@ import { WindowComponent, WindowConstraints, WindowDelegate } from '../../window
 })
 export class EditorComponent extends WindowComponent implements OnInit {
 
-  constructor() {
+  @ViewChild('editorTextfield', { static: true }) editorTextfield: ElementRef;
+
+  editorHtml: SafeHtml;
+
+  constructor(private fileService: FileService) {
     super();
   }
 
   ngOnInit() {
   }
 
+  enter(inputPath: string) {
+    console.log(inputPath);
+    let path: Path;
+    try {
+      path = Path.fromString(inputPath);
+    } catch {
+      console.log('The specified path is not valid');
+      return;
+    }
+
+    this.fileService.getFromPath(JSON.parse(sessionStorage.getItem('activeDevice')).uuid, path).subscribe(file => {
+      if (file.is_directory) {
+        console.log('That is not a file');
+      } else {
+        // TODO
+        console.log(file.content);
+        this.editorHtml = file.content;
+      }
+    }, error => {
+      if (error.message === 'file_not_found') {
+        console.log('That file does not exist');
+      } else {
+        console.log(error);
+      }
+    });
+  }
 }
 
 export class EditorWindowDelegate extends WindowDelegate {
