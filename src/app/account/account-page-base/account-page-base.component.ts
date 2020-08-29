2b230abdf5b684189f2ea04c7029adfba3d668ff
Marcel Koch <koch_marcel@outlook.com>
2020-08-30 01:50:20 +0200

    Removed particles (#77)

--- src/app/account/account-page-base/account-page-base.component.ts
+++ src/app/account/account-page-base/account-page-base.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import * as Particles from 'particlesjs/src/particles.js';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-account-page-base',
   templateUrl: './account-page-base.component.html',
   styleUrls: ['./account-page-base.component.scss']
 })
-export class AccountPageBaseComponent implements OnInit, OnDestroy {
-
-  private particles: Particles;
-
-  ngOnInit(): void {
-    this.particles = Particles.init({
-      selector: 'canvas',
-      maxParticles: 450,
-      color: '#007f00',
-      speed: 0.5
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.particles.destroy();
-  }
+export class AccountPageBaseComponent {
 }
