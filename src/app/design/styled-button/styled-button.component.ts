9e3d0b4f22ac3ac0686482d75f31881cd9426656
ClientCrash <clientcrashyt@gmail.com>
2021-09-18 14:02:30 +0200

    changed component structure

--- src/app/design/styled-button/styled-button.component.ts
+++ src/app/design/styled-button/styled-button.component.ts
@@ -5,38 +5,14 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./styled-button.component.scss']
 })
 export class StyledButtonComponent implements OnInit {
-  constructor(ststy: StandardStyle) {
-    switch (ststy) {
-      case StandardStyle.Primary:
-        this.buttonClass = 'primary';
-        break;
-      case StandardStyle.Warning:
-        this.buttonClass = 'warning';
-        break;
-      case StandardStyle.Error:
-        this.buttonClass = 'error';
-        break;
-      case StandardStyle.Success:
-        this.buttonClass = 'success';
-        break;
-      case StandardStyle.Info:
-        this.buttonClass = 'info';
-        break;
-      default:
-        this.buttonClass = 'primary';
-        break;
-    }
-  }
 
   @Input() disabled = false;
+  @Input() styleClass: string;
 
-
-  buttonClass = 'primary';
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
   @Input() onClick: (event: Event) => void = () => {};
-
 
 
 }
