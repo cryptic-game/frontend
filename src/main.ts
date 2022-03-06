0b295bd2ea56d7ed91f4fc16507a2eb8ad81f545
Tristan <triddel02@gmx.de>
2022-03-06 13:10:33 +0100

    Removal of service worker

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ if (environment.production) {
   enableProdMode();
 }
 
+navigator.serviceWorker.getRegistrations().then(registrations => {
+  for (const registration of registrations) {
+    registration.unregister().then(() => console.info('Unregistered service worker'));
+  }
+});
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err));
