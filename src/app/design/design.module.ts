26676a9aadcbdde2530ce8b15fe70ac1f437c0d9
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2022-03-06 11:20:17 +0100

    Added Button Base Component (#275)

--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {ButtonComponent} from './button/button.component';
 import {TextFieldComponent} from './text-field/text-field.component';
 import {FormGroupComponent} from './form-group/form-group.component';
+import {StyledButtonComponent} from './styled-button/styled-button.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     TextFieldComponent,
-    FormGroupComponent
+    FormGroupComponent,
+    StyledButtonComponent
   ],
   imports: [
     CommonModule
