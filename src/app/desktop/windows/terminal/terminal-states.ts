4fe977c9360741785ae9dc893955e8eb10064b79
McModknower <teetoll@t-online.de>
2022-03-07 23:06:12 +0100

    Use single quotes

--- src/app/desktop/windows/terminal/terminal-states.ts
+++ src/app/desktop/windows/terminal/terminal-states.ts
@@ -47,7 +47,7 @@ export abstract class CommandTerminalState implements TerminalState {
       // Only successful and not-flagged commands will be shown in the protocol.
       if (!this.commands[command].hideFromProtocol) {
         if (args.length>0) {
-          this.protocol.unshift(command + " " + args.join(" "));
+          this.protocol.unshift(command + ' ' + args.join(' '));
         } else {
           this.protocol.unshift(command);
         }
