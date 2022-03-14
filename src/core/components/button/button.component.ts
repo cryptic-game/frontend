529d50105f8378023776c452ba2ca549cf88e5c2
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-03-14 18:33:42 +0100

    Fixed merge conflicts

---
+++ src/core/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+export type ButtonFlavor = 'primary' | 'success' | 'warning' | 'danger' | 'info';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+
+  @Input() public disabled = false;
+  @Input() public flavor: ButtonFlavor = 'primary';
+
+  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
+  @Output() public onClick = new EventEmitter<MouseEvent>();
+}
---
+++ src/core/components/button/button.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ButtonComponent} from './button.component';
+
+@NgModule({
+  declarations: [
+    ButtonComponent
+  ],
+  exports: [
+    ButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonModule {
+}
