cde63f5dfbde17b0b69a6684b1564f1316ac1565
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-03-21 19:10:33 +0100

    Added some tags for sentry (#356)

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {init as Sentry} from "@sentry/angular";
+import {init as Sentry, setTag} from "@sentry/angular";
 
 if (environment.production) {
   Sentry({
     dsn: "https://201e994a90e7465495a4937fc36eb7c7@glitchtip.cryptic-game.net/7",
     autoSessionTracking: false,
+    
   });
+
+  setTag("host", location.host);
 
   enableProdMode();
 }
