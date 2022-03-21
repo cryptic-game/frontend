ed219e918d44596568ad1525874188661d352bae
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-03-21 11:52:35 +0100

    Added sentry (#354)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {ErrorHandler, NgModule, Provider} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
@@ -7,6 +7,37 @@ import {AppRouteReuseStrategy} from './app-route-reuse-strategy';
 import {ContextMenuModule} from "../core/components/context-menu/context-menu.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import * as Sentry from "@sentry/angular";
+import {environment} from 'src/environments/environment';
+
+const providers: Provider[] = [{provide: RouteReuseStrategy, useClass: AppRouteReuseStrategy}];
+
+if (environment.production) {
+  providers.push({
+    provide: ErrorHandler,
+    useValue: Sentry.createErrorHandler({
+      showDialog: true,
+      dialogOptions: {
+        onLoad: () => {
+          tryRemove('id_name', 'User');
+          tryRemove('id_email', 'user@cryptic-game.net');
+        }
+      }
+    })
+  });
+}
+
+function tryRemove(id: string, value: string) {
+  const emailInput = document.getElementById(id) as HTMLInputElement | null;
+
+  if (!emailInput) {
+    setTimeout(() => tryRemove(id, value), 100);
+    return;
+  }
+
+  emailInput.value = value;
+  emailInput.parentElement!.style.display = 'none';
+}
 
 @NgModule({
   declarations: [
@@ -19,9 +50,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     AppRoutingModule,
     ContextMenuModule.forRoot()
   ],
-  providers: [
-    {provide: RouteReuseStrategy, useClass: AppRouteReuseStrategy}
-  ],
+  providers,
   bootstrap: [AppComponent]
 })
 export class AppModule {
