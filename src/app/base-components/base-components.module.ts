a05098aa964b209532dce02e0ab292b1aa8e4bc4
Tristan-H11 <triddel02@gmx.de>
2022-03-22 19:43:12 +0100

    Moved Buttons into buttons-dir and fixed some paddings. (#279)

--- src/app/base-components/base-components.module.ts
+++ src/app/base-components/base-components.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { BaseComponentsRoutingModule } from './base-components-routing.module';
 import { BaseComponentsComponent } from './base-components.component';
-import {ButtonModule} from "../../core/components/button/button.module";
-import {ButtonOutlineModule} from "../../core/components/button-outline/button-outline.module";
-import {ButtonTextModule} from "../../core/components/button-text/button-text.module";
+import {ButtonModule} from "../../core/components/buttons/button/button.module";
+import {ButtonOutlineModule} from "../../core/components/buttons/button-outline/button-outline.module";
+import {ButtonTextModule} from "../../core/components/buttons/button-text/button-text.module";
 
 
 @NgModule({
