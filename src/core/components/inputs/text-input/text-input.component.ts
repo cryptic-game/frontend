405dbe22b0f8f60bd30e2f99cdc60f86dfb56de3
Tristan-H11 <triddel02@gmx.de>
2022-03-23 13:23:23 +0100

    Added flavor and placeholder-styling. (#288)

--- src/core/components/inputs/text-input/text-input.component.ts
+++ src/core/components/inputs/text-input/text-input.component.ts
@@ -1,5 +1,8 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
+export type Flavor = 'primary' | 'success' | 'warning' | 'danger' | 'info';
+
+
 @Component({
   selector: 'design-text-input',
   templateUrl: './text-input.component.html',
@@ -7,9 +10,13 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class TextInputComponent {
 
+  @Input() public disabled = false;
   @Input() placeholder: string = "Placeholder";
   @Input() hintText: string = "This is a small hint.";
   @Input() label: string = "Label";
+  @Input() hintColored: boolean = false;
+  @Input() public flavor: Flavor = 'primary';
+
 
   // eslint-disable-next-line @angular-eslint/no-output-on-prefix
   @Output() public onEnter = new EventEmitter<InputEvent>();
