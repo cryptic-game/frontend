7a3d05029408002f3301c527291c22dde1b7aef2
Louis Weigel <account@louisweigel.com>
2022-04-28 17:18:54 +0200

    nothing special just a test

--- src/app/desktop/windows/settings/settings.component.ts
+++ src/app/desktop/windows/settings/settings.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit, Type} from '@angular/core';
-import {WindowComponent, WindowConstraints, WindowDelegate} from '../../window/window-delegate';
-import {SettingsService} from './settings.service';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {SettingsEntry} from './settings-entry';
-import {forkJoin} from "rxjs";
-import {map} from "rxjs/operators";
+import { Component, OnInit, Type } from '@angular/core';
+import { WindowComponent, WindowConstraints, WindowDelegate } from '../../window/window-delegate';
+import { SettingsService } from './settings.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SettingsEntry } from './settings-entry';
+import { forkJoin } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-settings',
@@ -41,7 +41,7 @@ export class SettingsComponent extends WindowComponent implements OnInit {
   async loadForm() {
     forkJoin(this.settingEntries.map(
       ([name, setting]) => setting.getFresh().pipe(
-        map(value => ({name, value: value ?? setting.defaultValue}))
+        map(value => ({ name, value: value ?? setting.defaultValue }))
       )
     )).subscribe(values => {
       const result: { [name: string]: unknown } = {};
@@ -53,7 +53,7 @@ export class SettingsComponent extends WindowComponent implements OnInit {
   async resetSettings() {
     await Promise.all(this.settingEntries.map(async ([name, setting]) => {
       await setting.reset();
-      this.form.patchValue({[name]: setting.getCacheOrDefault()});
+      this.form.patchValue({ [name]: setting.getCacheOrDefault() });
     }));
   }
 
@@ -65,6 +65,14 @@ export class SettingsComponent extends WindowComponent implements OnInit {
     );
   }
 
+  // async saveOneSettings(setter: String) {
+  //   console.log(this.form.value)
+  //   console.log(this.settingEntries)
+  //   await Promise.all(
+  //     this.settingEntries.map(([name, setter]) => setter.set(this.form.value[name]))
+  //   );
+  // }
+
 }
 
 export class SettingsWindowDelegate extends WindowDelegate {
@@ -72,5 +80,5 @@ export class SettingsWindowDelegate extends WindowDelegate {
   icon = 'assets/desktop/img/gear.svg';
   type: Type<any> = SettingsComponent;
 
-  override constraints = new WindowConstraints({minWidth: 300, minHeight: 200});
+  override constraints = new WindowConstraints({ minWidth: 300, minHeight: 200 });
 }
