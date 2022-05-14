72f0935e6ece55d1da26ef07851ac3bae9305350
ClientCrash <40364569+ClientCrash@users.noreply.github.com>
2022-05-14 12:53:10 +0200

    moved declaration to ProgressBar Module

--- src/app/base-components/base-components.module.ts
+++ src/app/base-components/base-components.module.ts
@@ -6,12 +6,10 @@ import { BaseComponentsComponent } from './base-components.component';
 import {ButtonModule} from "../../core/components/buttons/button/button.module";
 import {ButtonOutlineModule} from "../../core/components/buttons/button-outline/button-outline.module";
 import {ButtonTextModule} from "../../core/components/buttons/button-text/button-text.module";
-import { ProgressbarComponent } from 'src/core/components/progressbar/progressbar.component';
 
 @NgModule({
   declarations: [
     BaseComponentsComponent,
-    ProgressbarComponent
   ],
   imports: [
     CommonModule,
